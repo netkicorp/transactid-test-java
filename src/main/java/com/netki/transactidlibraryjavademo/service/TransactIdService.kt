8b495f3f52e2ab706255d93dec2bfa434f3b352e
alan10fm <alan10fm@gmail.com>
2020-09-09 12:53:27 -0500

    Upgrading to latest version of TID library

--- src/main/java/com/netki/transactidlibraryjavademo/service/TransactIdService.kt
+++ src/main/java/com/netki/transactidlibraryjavademo/service/TransactIdService.kt
@@ -1,109 +1,74 @@
 package com.netki.transactidlibraryjavademo.service
 
 import com.netki.TransactId
-import com.netki.model.*
-import com.netki.transactidlibraryjavademo.repo.TransactIdRepository
-import com.netki.transactidlibraryjavademo.util.TestData
-import lombok.extern.slf4j.Slf4j
-import org.bouncycastle.jce.provider.BouncyCastleProvider
+import com.netki.transactidlibraryjavademo.util.TestData.Attestations.ATTESTATIONS_REQUESTED
+import com.netki.transactidlibraryjavademo.util.TestData.InvoiceRequest.INVOICE_REQUEST_DATA
+import com.netki.transactidlibraryjavademo.util.TestData.Owners.NO_PRIMARY_OWNER_PKI_X509SHA256
+import com.netki.transactidlibraryjavademo.util.TestData.Owners.PRIMARY_OWNER_PKI_X509SHA256
+import com.netki.transactidlibraryjavademo.util.TestData.Payment.PAYMENT_PARAMETERS
+import com.netki.transactidlibraryjavademo.util.TestData.PaymentRequest.PAYMENT_REQUEST_PARAMETERS
+import com.netki.transactidlibraryjavademo.util.TestData.Senders.SENDER_PKI_X509SHA256
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import java.security.Security
-import java.sql.Timestamp
-
 
 @Service
-@Slf4j
 class TransactIdService {
 
     private val logger = LoggerFactory.getLogger(this.javaClass)
+    private var transactId = TransactId.getInstance("src/main/resources/certificates")
+    val ownerParameters = listOf(PRIMARY_OWNER_PKI_X509SHA256, NO_PRIMARY_OWNER_PKI_X509SHA256)
 
-    val ownerParameters = listOf(
-        OwnerParameters(
-            isPrimaryForTransaction = true,
-            pkiDataParametersSets = listOf(
-                PkiDataParameters(
-                    attestation = "identification",
-                    privateKeyPem = TestData.CLIENT_PRIVATE_KEY,
-                    certificatePem = TestData.CLIENT_CERTIFICATE,
-                    type = PkiType.X509SHA256
-                )
-            )
+    fun getInitialInvoiceRequest(): ByteArray {
+        logger.info("Creating InvoiceRequest...")
+        val invoiceRequest = transactId.createInvoiceRequest(
+            INVOICE_REQUEST_DATA,
+            ownerParameters,
+            SENDER_PKI_X509SHA256,
+            ATTESTATIONS_REQUESTED
         )
-    )
-
-    val senderParameters = SenderParameters(
-        PkiDataParameters(
-            attestation = "identification",
-            privateKeyPem = TestData.SENDER_PRIVATE_KEY,
-            certificatePem = TestData.SENDER_CERTIFICATE,
-            type = PkiType.X509SHA256
-        )
-    )
-
-    fun getInitialInvoiceRequest(): ByteArray = createTestInvoice()
-
-    private fun createTestInvoice(): ByteArray {
-        val invoiceRequestParameters = InvoiceRequestParameters(
-            amount = 1000,
-            memo = "Payment for service",
-            notificationUrl = "www.notification.com/notify"
-        )
-
-        logger.info("Creating InvoiceRequest")
-        return TransactId.createInvoiceRequest(invoiceRequestParameters, ownerParameters, senderParameters)
+        logger.info("Returning InvoiceRequest...")
+        return invoiceRequest
     }
 
     fun postInvoiceRequest(invoiceRequest: ByteArray): ByteArray {
         logger.info("InvoiceRequest received")
-        logger.info("InvoiceRequest valid? ${TransactId.isInvoiceRequestValid(invoiceRequest)}")
-        val invoiceRequestModel = TransactId.parseInvoiceRequest(invoiceRequest)
+        logger.info("InvoiceRequest valid? ${transactId.isInvoiceRequestValid(invoiceRequest)}")
+        val invoiceRequestModel = transactId.parseInvoiceRequest(invoiceRequest)
         logger.info("InvoiceRequest parsed: $invoiceRequestModel")
-        val paymentDetails = PaymentParameters(
-            network = "main",
-            outputs = arrayListOf(
-                Output(100, "Script"),
-                Output(200, "Script")
-            ),
-            time = Timestamp(System.currentTimeMillis()),
-            expires = Timestamp(System.currentTimeMillis() + 1000),
-            memo = invoiceRequestModel.memo,
-            paymentUrl = invoiceRequestModel.notificationUrl,
-            merchantData = "Merchant data"
-        )
 
-        logger.info("Creating PaymentRequest")
-        return TransactId.createPaymentRequest(paymentDetails, ownerParameters, senderParameters, 1)
+        logger.info("Creating PaymentRequest...")
+        val paymentRequest = transactId.createPaymentRequest(
+            PAYMENT_REQUEST_PARAMETERS,
+            ownerParameters,
+            SENDER_PKI_X509SHA256,
+            ATTESTATIONS_REQUESTED,
+            1
+        )
+        logger.info("Returning PaymentRequest...")
+        return paymentRequest
     }
 
     fun postPaymentRequest(paymentRequest: ByteArray): ByteArray {
         logger.info("PaymentRequest received")
-        logger.info("PaymentRequest valid? ${TransactId.isPaymentRequestValid(paymentRequest)}")
-        val paymentRequestModel = TransactId.parsePaymentRequest(paymentRequest)
+        logger.info("PaymentRequest valid? ${transactId.isPaymentRequestValid(paymentRequest)}")
+        val paymentRequestModel = transactId.parsePaymentRequest(paymentRequest)
         logger.info("PaymentRequest parsed: $paymentRequestModel")
-        val paymentDetails = paymentRequestModel.paymentParameters
-        val paymentModel = Payment(
-            merchantData = paymentDetails.merchantData,
-            transactions = arrayListOf(
-                "transaction1".toByteArray(),
-                "transaction2".toByteArray()
-            ),
-            outputs = paymentDetails.outputs,
-            memo = paymentDetails.memo
-        )
 
-        logger.info("Creating Payment")
-        return TransactId.createPayment(paymentModel)
+        logger.info("Creating Payment...")
+        val payment = transactId.createPayment(PAYMENT_PARAMETERS, ownerParameters)
+        logger.info("Returning Payment...")
+        return payment
     }
 
     fun postPayment(payment: ByteArray): ByteArray {
         logger.info("Payment received")
-        logger.info("Payment valid? ${TransactId.isPaymentValid(payment)}")
-        val paymentModel = TransactId.parsePayment(payment)
+        logger.info("Payment valid? ${transactId.isPaymentValid(payment)}")
+        val paymentModel = transactId.parsePayment(payment)
         logger.info("Payment parsed: $paymentModel")
 
-        logger.info("Creating PaymentAck")
-        return TransactId.createPaymentAck(paymentModel, "Payment successful")
+        logger.info("Creating PaymentAck...")
+        val paymentAck = transactId.createPaymentAck(paymentModel, "Payment successful")
+        logger.info("Returning PaymentAck...")
+        return paymentAck
     }
 }
