d93dd492733780e09619e732a592ae8e58a75d30
alan10fm <alan10fm@gmail.com>
2020-12-14 14:19:28 -0600

    Upgrade to beta4

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.netki:transactid:1.0.0-beta3")
+    implementation("com.netki:transactid:1.0.0-beta4")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
